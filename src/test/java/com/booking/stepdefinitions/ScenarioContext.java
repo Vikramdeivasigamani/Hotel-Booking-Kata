4b2e42b36ad1fab28677e8a6c287a77440313efe
DESKTOP-SLRNDAB\WorkStation <brechtvanelst@hotmail.com>
2026-01-28 20:42:02 +0100

    Added BookingApi and created ScenarioContext for shared state.

--- src/test/java/com/booking/stepdefinitions/ScenarioContext.java
+++ src/test/java/com/booking/stepdefinitions/ScenarioContext.java
@@ -1,8 +1,11 @@
 package com.booking.stepdefinitions;
 
+import io.restassured.response.Response;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class BaseTest {
     public static List<Integer> bookingIds = new ArrayList<>();
+    public Response response;
 }
